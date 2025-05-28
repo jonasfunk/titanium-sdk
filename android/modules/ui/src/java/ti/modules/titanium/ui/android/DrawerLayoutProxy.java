37009ac799e04fa3a158583c1fb280d284634da1
jonasfunk <jonasfunk@users.noreply.github.com>
2025-05-28 12:27:26 +0200

    fix: remove unused clipMode settings in TiUIView initialization

--- android/modules/ui/src/java/ti/modules/titanium/ui/android/DrawerLayoutProxy.java
+++ android/modules/ui/src/java/ti/modules/titanium/ui/android/DrawerLayoutProxy.java
@@ -14,6 +14,7 @@ import org.appcelerator.titanium.TiC;
 import org.appcelerator.titanium.view.TiUIView;
 
 import android.app.Activity;
+import android.view.View;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 @Kroll.proxy(creatableInModule = AndroidModule.class)
